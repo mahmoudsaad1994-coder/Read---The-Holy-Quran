034a52246778795029e8b8da37bd12d66474c90f
mahmoudsaad1994-coder <mahmodsaad22@gmail.com>
2024-08-01 02:56:50 +0300

    complete splash screen

--- lib/feature/splash/presentation/view/widgets/splash_view_body.dart
+++ lib/feature/splash/presentation/view/widgets/splash_view_body.dart
@@ -15,6 +15,7 @@ class SplashViewBody extends StatefulWidget {
 class _SplashViewBodyState extends State<SplashViewBody>
     with SingleTickerProviderStateMixin {
   late AnimationController animationController;
+  late Animation<Offset> animation;
 
   @override
   void initState() {
@@ -35,19 +36,63 @@ class _SplashViewBodyState extends State<SplashViewBody>
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SafeArea(
-          child: Center(
-        child: FadeTransition(
-          opacity: animationController.drive(
-            CurveTween(curve: Curves.easeIn),
-          ),
-          child: Image.asset(
-            'assets/images/grad_logo.png',
-            fit: BoxFit.fill,
-            height: size.height * .3,
+      body: Container(
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(255, 132, 192, 205),
+              Color.fromARGB(255, 30, 110, 107)
+            ],
           ),
         ),
-      )),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            FadeTransition(
+              opacity: animationController.drive(
+                CurveTween(curve: Curves.easeIn),
+              ),
+              child: Image.asset(
+                'assets/images/grad_logo.png',
+                fit: BoxFit.fill,
+                height: size.height * .3,
+              ),
+            ),
+            FadeTransition(
+              opacity: animationController.drive(
+                CurveTween(curve: Curves.easeIn),
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(bottom: size.height * .05),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'صدقة جارية عن والدي نسألكم الدعاء',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white54,
+                        fontSize: size.width * .04,
+                      ),
+                    ),
+                    Text(
+                      'سعدالدين حامد عبدالعال الملا',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white54,
+                        fontSize: size.width * .04,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 
@@ -58,7 +103,8 @@ class _SplashViewBodyState extends State<SplashViewBody>
       lowerBound: 0.0,
       upperBound: 1,
     );
-
+    animation = Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+        .animate(animationController);
     animationController.forward();
   }
 
